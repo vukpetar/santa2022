ef1b1155b8d3f5521e15bbf384fbe38f7875b3c8
Petar Sekulić <petar77@live.com>
2023-01-13 20:34:14 +0100

    Add finetune_agent_with_ppo notebook

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
 from functools import reduce
-from typing import Union, List, Tuple, Iterator
+from typing import (
+    Union,
+    List,
+    Dict,
+    Tuple,
+    Iterator,
+    Type,
+    Callable,
+    Optional
+)
 import base64
 from pathlib import Path
 
@@ -8,8 +17,14 @@ import pandas as pd
 import numba as nb
 import scipy.signal
 
+import torch as th
+from torch import nn
+from torch.utils.data import Dataset
+
 import gym
 from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.ppo.ppo import PPO
 from IPython import display as ipythondisplay
 
@@ -308,3 +323,210 @@ def discounted_cumulative_sums(x: np.array, discount: float) -> np.array:
 
     advantages = scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
     return advantages
+
+class FeaturesExtractorCNN(BaseFeaturesExtractor):
+    """
+    :param observation_space: (gym.Space)
+    :param features_dim: (int) Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    """
+
+    def __init__(
+        self,
+        observation_space: Dict,
+        features_dim: int = 128,
+        n_input_channels: int = 3
+    ):
+        super(FeaturesExtractorCNN, self).__init__(observation_space, features_dim)
+        
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with th.no_grad():
+            n_flatten = self.cnn(
+                th.rand(1, 3, 257, 257).float()
+            ).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+        
+        self.conf_linear = nn.Sequential(nn.Linear(16, features_dim), nn.ReLU())
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        image = observations["image"].permute(0, 3, 1, 2)
+        conf = observations["conf"]
+        
+        image_features = self.linear(self.cnn(image))
+        conf_features = self.conf_linear(conf)
+        
+        features = th.cat((image_features, conf_features), 1)
+        return features
+
+class CustomPPONetwork(nn.Module):
+    """
+    Custom network for policy and value function.
+    It receives as input the features extracted by the features extractor.
+
+    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    """
+
+    def __init__(
+        self,
+        feature_dim: int,
+        last_layer_dim_pi: int = 3**8,
+        last_layer_dim_vf: int = 1,
+    ):
+        super().__init__()
+        
+        self.latent_dim_pi = last_layer_dim_pi
+        self.latent_dim_vf = last_layer_dim_vf
+
+        # Policy network
+        self.policy_net = nn.Sequential(
+            nn.Linear(feature_dim*2, feature_dim*4), nn.ReLU(),
+            nn.Linear(feature_dim*4, feature_dim*8), nn.ReLU(),
+            nn.Linear(feature_dim*8, last_layer_dim_pi)
+        )
+        # Value network
+        self.value_net = nn.Sequential(
+            nn.Linear(feature_dim*2, feature_dim), nn.ReLU(),
+            nn.Linear(feature_dim, feature_dim//2), nn.ReLU(),
+            nn.Linear(feature_dim//2, last_layer_dim_vf)
+        )
+
+    def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features: th.Tensor) -> th.Tensor:
+        return self.policy_net(features)
+
+    def forward_critic(self, features: th.Tensor) -> th.Tensor:
+        return self.value_net(features)
+
+class CustomActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Callable[[float], float],
+        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+        activation_fn: Type[nn.Module] = nn.Tanh,
+        *args,
+        **kwargs,
+    ):
+
+        super(CustomActorCriticPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            # Pass remaining arguments to base class
+            *args,
+            **kwargs,
+        )
+        # Disable orthogonal initialization
+        self.ortho_init = False
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = CustomPPONetwork(self.features_dim)
+
+class CustomNetwork(nn.Module):
+    """
+    Custom network for policy and value function.
+    It receives as input the features extracted by the feature extractor.
+
+    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    """
+
+    def __init__(
+        self,
+        feature_dim: int,
+        n_input_channels: int = 3,
+        last_layer_dim_pi: int = 64,
+        last_layer_dim_vf: int = 64
+    ):
+        super(CustomNetwork, self).__init__()
+        
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with th.no_grad():
+            n_flatten = self.cnn(
+                th.rand(1, 3, 257, 257).float()
+            ).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, feature_dim), nn.ReLU())
+        
+        self.conf_linear = nn.Sequential(nn.Linear(16, feature_dim), nn.ReLU())
+        
+        
+
+        # Policy network
+        self.policy_net = nn.Sequential(
+            nn.Linear(feature_dim*2, last_layer_dim_pi)
+        )
+        # Value network
+        self.value_net = nn.Sequential(
+            nn.Linear(feature_dim*2, last_layer_dim_vf)
+        )
+
+    def forward(self, images: th.Tensor, confs: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+
+        image_features = self.linear(self.cnn(images))
+        conf_features = self.conf_linear(confs)
+        
+        features = th.cat((image_features, conf_features), 1)
+
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features: th.Tensor) -> th.Tensor:
+        return self.policy_net(features)
+
+    def forward_critic(self, features: th.Tensor) -> th.Tensor:
+        return self.value_net(features)
+
+class SantaDataset(Dataset):
+    """Santa dataset."""
+
+    def __init__(self, observations, actions, values):
+
+        self.observations = observations
+        self.actions = actions
+        self.values = values
+
+    def __len__(self):
+        return len(self.observations)
+
+    def __getitem__(self, idx):
+        observation = self.observations[idx]
+        image = observation["image"]
+        conf = observation["conf"]
+        action = self.actions[idx]
+        reward = self.values[idx]
+
+        return image, conf, action, reward
